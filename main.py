8ebaf3d24376af24c80d1c5569e70c8fc8c500ef
doron tiferet <sdoron5.t2@gmail.com>
2017-08-25 00:04:32 +0300

    url duplication handle

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import appearances
 import re
 from flask import jsonify
+import terms_provider
+# encoding=utf8
+import sys
 
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 from flask import Flask
 app = Flask(__name__)
@@ -9,16 +14,16 @@ app = Flask(__name__)
 def remove_parentheses(str):
     return re.sub(r"[\(\[].*?[\)\]]", "", str)
 
+
+
 @app.route("/")
 def asdf():
     return "asdf"
 
 @app.route("/<subject>")
 def hello(subject):
-    print subject
-    subject = 'computer science'
-    terms = ['GRASP', 'SOLID']
-
+    terms = terms_provider.get_final_terms(subject, 0)
+    print 'terms count: ' + str(len(terms))
     terms = list(map(lambda x: remove_parentheses(x).lower().strip(), terms))
     y = appearances.build_appearances_dict(subject, terms)
     x = 4
