bc0ec4607d5146a6e0b09abbd989f1d42019e24f
Your Name <you@example.com>
2017-08-25 09:26:36 +0300

    added better version

--- backend/appearances.py
+++ backend/appearances.py
@@ -4,41 +4,20 @@ from bs4 import BeautifulSoup
 
 
 URL_LIMIT = 3
-def build_appearances_dict(args):
-    term, all_terms, subject = args
+def build_appearances_dict(response, term, all_terms):
     appearances_dict = {}
     appearances_dict[term] = {}
-    search_results = urls_scanner.get_urls(subject + " " + term, URL_LIMIT)
-    safe_urls = []
-    seen_urls = []
 
-    for url in search_results:
-        baseUrl = url.split("#")[0]
-        if not(baseUrl in seen_urls):
-            seen_urls.append(baseUrl)
-            safe_urls.append(url)
-            if len(safe_urls) >= URL_LIMIT:
-                break
-
-    for url in safe_urls:
-        try:
-            print "scanning url " + url
-            response = requests.get(url, verify=False)
-        except BaseException as e:
-            print e
-            continue
-        if response.status_code == 200:
-            print "finished scanning " + url
-            data = response.text
-            page_content = str(BeautifulSoup(data).text)
-            for term2 in all_terms:
-                if term2 != term:
-                    appearances_count = count_substring_in_string(page_content, term2)
-                    if appearances_count >= 0:
-                        if term2 in appearances_dict[term]:
-                            appearances_dict[term][term2] += appearances_count
-                        else:
-                            appearances_dict[term][term2] = appearances_count
+    data = response.text
+    page_content = str(BeautifulSoup(data).text)
+    for term2 in all_terms:
+        if term2 != term:
+            appearances_count = count_substring_in_string(page_content, term2)
+            if appearances_count >= 0:
+                if term2 in appearances_dict[term]:
+                    appearances_dict[term][term2] += appearances_count
+                else:
+                    appearances_dict[term][term2] = appearances_count
 
 
     return appearances_dict
--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,8 @@ import terms_provider
 # encoding=utf8
 import sys
 from multiprocessing.dummy import Pool as ThreadPool
+import grequests
+import urls_scanner
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -23,15 +25,42 @@ def asdf():
 def hello(subject):
     terms = map(lambda x:remove_parentheses(x).lower().strip(), list(terms_provider.get_final_terms(subject, 0)))
     print 'terms count: ' + str(len(terms))
-    terms_passed = list(map(lambda x: (x, terms, subject), terms))
-    print terms_passed
-    pool = ThreadPool(20)
-    results = pool.map(appearances.build_appearances_dict, terms_passed)
+
+    # urls = get_urls(subject, terms)
+    urls = get_urls(subject, terms)
+    rs = (grequests.get(u, timeout=3) for u in map(lambda x: x, urls.keys()))
+    responses = grequests.map(rs)
+    results = []
+    for response in responses:
+        if response is None:
+            continue
+        response_url = response.url
+        if response_url not in urls:
+            response_url = response_url.replace("https", "http")
+            if response_url not in urls:
+                continue
+        term = urls[response_url]
+        result = appearances.build_appearances_dict(response, term, terms)
+        results.append(result)
+
     newResults = {}
     for result in results:
         key = result.keys()[0]
         newResults[key] = result[key]
 
     return jsonify(newResults)
+
+
+def get_urls(subject, terms):
+    urls = {}
+    URL_LIMIT = 3
+    for term in terms:
+        search_results = urls_scanner.get_urls(subject + " " + term, URL_LIMIT)
+        for search_result in search_results:
+            if search_result in urls:
+                continue
+            urls[search_result] = term
+    return urls
+
 
 app.run()
