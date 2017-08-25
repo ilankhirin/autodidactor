dc47fc4d8a9571abf0f74d635a37d79baf486494
TamirSh <tamir2341@gmail.com>
2017-08-25 04:31:53 +0300

    Added autocomlete infrastructure and some refines

--- Client/all.js
+++ Client/all.js
@@ -1,5 +1,14 @@
-var BACKEND_URL = "http://fake";
-
+var BACKEND_URL = "http://localhost:5000";
+var SUBJECT_AUTOCOMPLETE_URL = "https://en.wikipedia.org/w/api.php";
+var SUBJECT_AUTOCOMPLETE_PARAMS = {
+    action: "opensearch",
+    format: "json",
+    formatversion: 2,
+    search: "",
+    namespace: 0,
+    limit: 10,
+    suggest: true
+};
 
 angular.module("autodidactorApp", []);
 
@@ -7,16 +16,29 @@ angular.module("autodidactorApp").controller("autodidactorCtrl", function($http)
     var autodidactorVm = this;
 
     autodidactorVm.searchInput = "Enter subject...";
+    autodidactorVm.graphJson = "Not back yet...";
 
     autodidactorVm.search = function(inputSubject) {
-        url = BACKEND_URL + "/getGraph";
-        $http.get(url, {
-            params: {
-                subject: inputSubject
-            }
-        }).then(function(data) {
+        url = BACKEND_URL + "/getGraph/" + inputSubject;
+        $http.get(url).then(function(data) {
             console.log(data);
+            autodidactorVm.graphJson = data;
         });
+    }
+
+    autodidactorVm.getSubjectAutocomlete = function(inputText) {
+        console.log(inputText);
+        debugger;
+        url = BACKEND_URL + "/getAutocomplete/" + inputText;
+
+        SUBJECT_AUTOCOMPLETE_PARAMS.search = inputText;
+        $http.get(url)
+            .then(function(res) {
+                debugger;
+            },
+            function(res) {
+                debugger;
+            });
     }
 
     function initializeGraph(graphData) {
