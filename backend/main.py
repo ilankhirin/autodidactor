a28f6776ba5ed6e6c48aa4a4bd21498010847619
TamirSh <tamir2341@gmail.com>
2017-08-25 06:16:38 +0300

    Really fixed conficts in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sys
 import re
 from flask import jsonify, Flask
@@ -41,43 +40,3 @@ def getAutocomplete(input):
     options = categoryAutocomleter.getOptions(input)
     optionsDict = { "options" : options }
     return jsonify(optionsDict)
-
-=======
-import appearances
-import re
-from flask import jsonify
-import terms_provider
-# encoding=utf8
-import sys
-from multiprocessing.dummy import Pool as ThreadPool
-
-reload(sys)
-sys.setdefaultencoding('utf8')
-
-from flask import Flask
-app = Flask(__name__)
-
-def remove_parentheses(str):
-    return re.sub(r"[\(\[].*?[\)\]]", "", str)
-
-@app.route("/")
-def asdf():
-    return "asdf"
-
-@app.route("/<subject>")
-def hello(subject):
-    terms = map(lambda x:remove_parentheses(x).lower().strip(), list(terms_provider.get_final_terms(subject, 0)))
-    print 'terms count: ' + str(len(terms))
-    terms_passed = list(map(lambda x: (x, terms, subject), terms))
-    print terms_passed
-    pool = ThreadPool(100)
-    results = pool.map(appearances.build_appearances_dict, terms_passed)
-    newResults = {}
-    for result in results:
-        key = result.keys()[0]
-        newResults[key] = result[key]
-
-    return jsonify(newResults)
-
->>>>>>> 519f26368fc48d0aa96f82b275a9573bfb926a00
-app.run()
